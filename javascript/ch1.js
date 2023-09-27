dafc1d0e4244b3128486b528951ad004371c6ea0
jackwarren430 <90930942+jackwarren430@users.noreply.github.com>
2023-09-27 09:07:57 -0400

    working on ch 1

---
+++ javascript/ch1.js
@@ -0,0 +1,4 @@
+document.getElementById('menu-button').addEventListener('click', function() {
+  const overlayMenu = document.getElementById('overlay-menu');
+  overlayMenu.classList.toggle('hidden');
+});
--- javascript/main.js
+++ javascript/main.js
@@ -6,10 +6,10 @@ document.addEventListener('DOMContentLoaded', function () {
     const div = document.createElement('div');
     div.className = 'particle';
     const rect = element.getBoundingClientRect();
-    const size = Math.random() * 0.04 * rect.width + 5;
+    const size = Math.random() * 0.06 * rect.width + 5;
     const x = rect.left + rect.width / 2;
     const y = rect.top + rect.height / 2;
-    const distance = rect.width * 0.8; 
+    const distance = rect.width * 1; 
     const radians = angle * (Math.PI / 180);
     const startX = x + Math.cos(radians) * distance - size / 2;
     const startY = (y + Math.sin(radians) * distance - size / 2);
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   function animateParticle(particle, startX, startY, startAngle) {
-    const speed = 0.3; 
+    const speed = 0.5; 
     const duration = 2000;
     const startTime = Date.now();
     const startRadians = startAngle * (Math.PI / 180);
@@ -57,5 +57,5 @@ document.addEventListener('DOMContentLoaded', function () {
   setInterval(() => {
     angle = Math.random() * 360;
     createParticle(angle);
-  }, 50);
+  }, 30);
 });
