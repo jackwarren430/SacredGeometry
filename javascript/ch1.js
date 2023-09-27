79849672f97d4f0edab96d3034b35f298fbf126e
jackwarren430 <90930942+jackwarren430@users.noreply.github.com>
2023-09-27 19:31:01 -0400

    fixed drop down menu

--- javascript/ch1.js
+++ javascript/ch1.js
@@ -1,4 +1,6 @@
 document.getElementById('menu-button').addEventListener('click', function() {
   const overlayMenu = document.getElementById('overlay-menu');
+  const arrowImg = document.getElementById('arrow-img');
   overlayMenu.classList.toggle('hidden');
+  arrowImg.classList.toggle('rotate');
 });
