5eeda4e21a540cc204ead3b91390e0ca392b73a8
jackwarren430 <90930942+jackwarren430@users.noreply.github.com>
2023-10-03 19:47:24 -0400

    working on ch1

--- javascript/ch1.js
+++ javascript/ch1.js
@@ -4,3 +4,34 @@ document.getElementById('menu-button').addEventListener('click', function() {
   overlayMenu.classList.toggle('hidden');
   arrowImg.classList.toggle('rotate');
 });
+
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function fadeIn(text) {
+  text.classList.add('fade-in');
+  setTimeout(() => fadeOut(text), 2000);
+}
+
+function fadeOut(text) {
+  text.classList.remove('fade-in');
+}
+
+const introText = document.getElementById('intro-text');
+setTimeout(() => fadeIn(introText), 500);
+
+
+//chapter 1
+
+setTimeout(chapterOne, 4000);
+
+const mainText = document.getElementById('box-text');
+async function chapterOne() {
+  mainText.innerText = "In the beginning, there was nothing";
+  fadeIn(mainText);
+  delay(6000);
+  mainText.classList.add("italicized");
+  mainText.innerText = "Click to create a point";
+  mainText.classList.add('fade-in');
+}
